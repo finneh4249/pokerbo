dfbf6f0a7378a568c5cf5676af35caa8c578ff01
finneh4249 <ethancornwill@outlook.com>
2024-02-20 18:54:28 +1100

    ESLint!

---
+++ assets/js/serviceWorker.js
@@ -0,0 +1,32 @@
+const staticPokerBo = 'pokerbo'
+const assets = [
+  '/',
+  '/index.html',
+  'assets/css/pokerbo.css',
+  'assets/js/main.js'
+]
+
+self.addEventListener('install', installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticPokerBo).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener('fetch', fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker
+      .register('/serviceWorker.js')
+      .then(res => console.log('service worker registered'))
+      .catch(err => console.log('service worker not registered', err))
+  })
+}
