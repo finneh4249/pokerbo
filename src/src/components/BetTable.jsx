f7f5697cc23b5eaf7de8dace0dbe8e0c803e4838
Ethan Cornwill <ethancornwill@outlook.com>
2024-11-26 13:40:22 +1100

    Refactor BetTable component: simplify layout and integrate DealerCards component

--- src/src/components/BetTable.jsx
+++ src/src/components/BetTable.jsx
@@ -2,23 +2,15 @@ import React, {Component} from 'react';
 import SomethingElse from './SomethingElse';
 import PlaceBets from './BetButtons/PlaceBets';
 import BetButtons from './BetButtons/BetButtons';
+import DealerCards from './DealerCards';
 
 export default class BetTable extends Component {
     render() {
         return (
             <>
-            <div class="container" id="table">
-  
-            <div class="d-grid mx-auto my-auto col-12">
-              <img src="assets/images/pokerbo.webp" width="400" class="center" alt='logo' />
-              <div id="dealer-cards" ></div>
-            </div>
-            <img src="assets/images/pokerbo.webp" width="400" class="center" alt='logo' />
-            <hr />
-                <BetButtons />
-              <br />
-              <PlaceBets />
-            </div>
+            <BetButtons />
+            <br />
+            <PlaceBets />
             <SomethingElse />
             </>    
         );
